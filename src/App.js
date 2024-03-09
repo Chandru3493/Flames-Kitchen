96c03f1b7528fb1aa5ebba6b6d2adba065ce402c
Anurag14092001 <www.anuragbhattacharjee99@gmail.com>
2024-03-09 13:01:06 +0530

    full app merges

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // App.js
 import React, { useState } from 'react';
 import './waiter.css'; 
-import RestaurantHeader from './components/RestaurantHeader';
-import TableArea from './components/TableArea'; 
-import NotificationsBoard from './components/NotificationsBoard';
-import TableDetailsPopup from './components/TableDetailsPopup';
+import RestaurantHeader from './Components/RestaurantHeader';
+import TableArea from './Components/TableArea'; 
+import NotificationsBoard from './Components/NotificationsBoard';
+import TableDetailsPopup from './Components/TableDetailsPopup';
 
 function App() {
   const [selectedTable, setSelectedTable] = useState(null);
